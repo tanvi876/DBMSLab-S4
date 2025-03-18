0712d3f6e35b5a7358c953341c0ca8452ab0f1c1
tanvi876 <n.sritanvirajavarapu@gmail.com>
2025-03-18 21:13:36 +0530

    ..

---
+++ NITCbase/mynitcbase/Buffer/StaticBuffer.cpp
@@ -0,0 +1,149 @@
+#include "StaticBuffer.h"
+
+unsigned char StaticBuffer::blocks[BUFFER_CAPACITY][BLOCK_SIZE];
+struct BufferMetaInfo StaticBuffer::metainfo[BUFFER_CAPACITY];
+unsigned char StaticBuffer::blockAllocMap[DISK_BLOCKS];
+
+StaticBuffer::StaticBuffer(){
+    // initialise all blocks as free
+    // Stage 6: set other parameters in the metainfo
+
+    // copy blockAllocMap blocks from disk to buffer (using readblock() of disk)
+  // blocks 0 to 3
+    for(int i=0,blockMapslot=0;i<4;i++){
+       unsigned char buffer[BLOCK_SIZE];
+       Disk::readBlock(buffer, i);
+       for(int slot=0;slot<BLOCK_SIZE;slot++,blockMapslot++){
+        StaticBuffer::blockAllocMap[blockMapslot] = buffer[slot];
+       }
+    }
+
+    for(int bufferIndex = 0; bufferIndex<BUFFER_CAPACITY; bufferIndex++){
+        metainfo[bufferIndex].free = true;
+        metainfo[bufferIndex].dirty = false;
+        metainfo[bufferIndex].blockNum = -1;
+        metainfo[bufferIndex].timeStamp = -1;
+    }
+}
+
+/*
+    At this stage, we are not writing back from the buffer to the disk since we are
+    not modifying the buffer. So, we will define an empty destructor for now, In
+    subsequent stages, we will implement the write-back functionality here.
+*/
+StaticBuffer::~StaticBuffer(){
+
+    //copy blockALlocMap blocks from buffer to disk
+    for(int i=0,blockMapslot=0;i<4;i++){
+        unsigned char buffer[BLOCK_SIZE];
+        for(int slot=0;slot<BLOCK_SIZE;slot++,blockMapslot++){
+         buffer[slot] = blockAllocMap[blockMapslot];
+        }
+        Disk::writeBlock(buffer, i);
+     }
+    
+    /*
+        Iterate through all the buffer block,
+        write back blocks with metainfo as free = false, dirty = true using Disk::writeBlock()
+    */
+    for(int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++){
+        if(metainfo[bufferIndex].free == false 
+               && metainfo[bufferIndex].dirty == true){
+            Disk::writeBlock(StaticBuffer:: blocks[bufferIndex], metainfo[bufferIndex].blockNum);
+        }
+    }
+}
+
+/*
+    Assigns a buffer to the block and returns the buffer number.
+    If no free buffer block is found, the least recently used buffer block
+    is replaced.
+*/
+/*
+    The timeStamp is reset to 0 each time the buffer block is accessed and
+    incremented when other buffer blocks are accessed.
+*/
+int StaticBuffer::getFreeBuffer(int blockNum){
+    if(blockNum < 0 || blockNum >= DISK_BLOCKS){
+        return E_OUTOFBOUND;
+    }
+
+    int bufferNum = -1, bufferWithMaxTimeStamp = -1, maxTimeStamp = 0;
+    // increase the timeStamp in the metaInfo of all occupied buffers.
+    // if a free buffer is not avaliable
+    //      find the buffer with largest timeStamp
+    //      if it is DIRTY, write back to the disk
+    //      set bufferNum = index of this buffer
+    for(int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++){
+        if(metainfo[bufferIndex].free == false){
+            metainfo[bufferIndex].timeStamp += 1;
+
+            if(maxTimeStamp < metainfo[bufferIndex].timeStamp){
+                bufferWithMaxTimeStamp = bufferIndex;
+                maxTimeStamp = metainfo[bufferIndex].timeStamp;
+            }
+        }
+
+        // if free buffer is avaliable, set bufferNum = bufferIndex
+        if(metainfo[bufferIndex].free == true && bufferNum == -1){
+            bufferNum = bufferIndex;
+        }
+    }
+
+    // free buffer not found
+    if(bufferNum == -1){
+        if(metainfo[bufferWithMaxTimeStamp].dirty == true){
+            Disk::writeBlock(StaticBuffer:: blocks[bufferWithMaxTimeStamp], metainfo[bufferWithMaxTimeStamp].blockNum);
+        }
+        bufferNum = bufferWithMaxTimeStamp;
+    }
+
+    // update the metaInfo entry correspinding to bufferNum
+    metainfo[bufferNum].free = false;
+    metainfo[bufferNum].dirty = false;
+    metainfo[bufferNum].blockNum = blockNum;
+    metainfo[bufferNum].timeStamp = 0;
+
+    return bufferNum;
+}
+
+/*
+    Get the buffer index where a particular block is stored
+    or E_BLOCKNOTINBUFFER otherwise
+*/
+int StaticBuffer::getBufferNum(int blockNum){
+    // check if block is valid (b/w 0 and DISK_BLOCKS)
+    if(blockNum < 0 || blockNum >= DISK_BLOCKS){
+        return E_OUTOFBOUND;
+    }
+
+    // if found, return the bufferIndex which corresponds to blockNum
+    for(int i = 0; i < BUFFER_CAPACITY; i++){
+        if(metainfo[i].blockNum == blockNum && metainfo[i].free == false){
+            return i;
+        }
+    }
+
+    return E_BLOCKNOTINBUFFER;
+}
+
+/*
+    setDirtyBit(): sets the dirty bit of the buffer corresponding to the block
+*/
+int StaticBuffer::setDirtyBit(int blockNum){
+    // find the buffer index corresponding to the block using getBufferNum()
+    int bufferNum = StaticBuffer::getBufferNum(blockNum);
+
+    if(bufferNum == E_BLOCKNOTINBUFFER){
+        return E_BLOCKNOTINBUFFER;
+    }
+
+    if(bufferNum == E_OUTOFBOUND){
+        return E_OUTOFBOUND;
+    }
+
+    // else the buffer is found, set the dirty bit.
+    metainfo[bufferNum].dirty = true;
+    
+    return SUCCESS;
+}
---
+++ NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
@@ -0,0 +1,97 @@
+#include "AttrCacheTable.h"
+
+#include <cstring>
+#include <stdio.h>
+
+AttrCacheEntry *AttrCacheTable::attrCache[MAX_OPEN];
+
+/* returns the attrOffset-th attribute for the relation corresponding to relId
+NOTE: this function expects the caller to allocate memory for `*attrCatBuf`
+*/
+int AttrCacheTable::getAttrCatEntry(int relId, int attrOffset,AttrCatEntry *attrCatBuf) {
+  // check if 0 <= relId < MAX_OPEN and return E_OUTOFBOUND otherwise
+  if (relId < 0 || relId >= MAX_OPEN) {
+    return E_OUTOFBOUND;
+  }
+
+  // check if attrCache[relId] == nullptr and return E_RELNOTOPEN if true
+  if (attrCache[relId] == nullptr) {
+    return E_RELNOTOPEN;
+  }
+  // traverse the linked list of attribute cache entries
+
+  for (AttrCacheEntry *entry = attrCache[relId]; entry != nullptr;
+       entry = entry->next) {
+    if (entry->attrCatEntry.offset == attrOffset) {
+      // copy entry->attrCatEntry to *attrCatBuf and return SUCCESS;
+      *attrCatBuf = entry->attrCatEntry;
+    }
+  }
+
+  // there is no attribute at this offset
+  return E_ATTRNOTEXIST;
+}
+
+/* Converts a attribute catalog record to AttrCatEntry struct
+    We get the record as Attribute[] from the BlockBuffer.getRecord() function.
+    This function will convert that to a struct AttrCatEntry type.
+*/
+
+/* returns the attribute with name `attrName` for the relation corresponding to
+relId NOTE: this function expects the caller to allocate memory for
+`*attrCatBuf`
+*/
+int AttrCacheTable::getAttrCatEntry(int relId, char attrName[ATTR_SIZE],AttrCatEntry *attrCatBuf) {
+  // check that relId is valid and corresponds to an open relation
+  if (relId < 0 || relId >= MAX_OPEN) {
+    return E_OUTOFBOUND;
+  }
+
+  // check if attrCache[relId] == nullptr and return E_RELNOTOPEN if true
+  if (attrCache[relId] == nullptr) {
+    return E_RELNOTOPEN;
+  }
+  // for (AttrCacheEntry *entry = attrCache[relId]; entry != nullptr;entry = entry->next) {
+  //   if (strcmp(entry->attrCatEntry.attrName, attrName) == 0) {
+  //     strcpy(attrCatBuf->relName, entry->attrCatEntry.relName);
+  //     strcpy(attrCatBuf->attrName, entry->attrCatEntry.attrName);
+  //     attrCatBuf->attrType = entry->attrCatEntry.attrType;
+  //     attrCatBuf->offset = entry->attrCatEntry.offset;
+  //     attrCatBuf->primaryFlag = entry->attrCatEntry.primaryFlag;
+  //     attrCatBuf->rootBlock = entry->attrCatEntry.rootBlock;
+  //     return SUCCESS;
+  //   }
+  // }
+
+  for (AttrCacheEntry *entry =attrCache[relId]; entry != nullptr;entry = entry->next) {
+
+    if (strcmp(entry->attrCatEntry.attrName, attrName) == 0) {
+      strcpy(attrCatBuf->relName, entry->attrCatEntry.relName);
+      strcpy(attrCatBuf->attrName, entry->attrCatEntry.attrName);
+      attrCatBuf->attrType = entry->attrCatEntry.attrType;
+      attrCatBuf->offset = entry->attrCatEntry.offset;
+      attrCatBuf->primaryFlag = entry->attrCatEntry.primaryFlag;
+      attrCatBuf->rootBlock = entry->attrCatEntry.rootBlock;
+      return SUCCESS;
+    }
+  }
+  // iterate over the entries in the attribute cache and set attrCatBuf to the
+  // entry that
+  //    matches attrName
+
+  // no attribute with name attrName for the relation
+  return E_ATTRNOTEXIST;
+}
+
+
+void AttrCacheTable::recordToAttrCatEntry(
+    union Attribute record[ATTRCAT_NO_ATTRS], AttrCatEntry *attrCatEntry) {
+  strcpy(attrCatEntry->relName, record[ATTRCAT_REL_NAME_INDEX].sVal);
+  strcpy(attrCatEntry->attrName, record[ATTRCAT_ATTR_NAME_INDEX].sVal);
+  attrCatEntry->offset = (int)record[ATTRCAT_OFFSET_INDEX].nVal;
+  attrCatEntry->attrType = (int)record[ATTRCAT_ATTR_TYPE_INDEX].nVal;
+  attrCatEntry->rootBlock = (int)record[ATTRCAT_ROOT_BLOCK_INDEX].nVal;
+  attrCatEntry->primaryFlag = (bool)record[ATTRCAT_PRIMARY_FLAG_INDEX].nVal;
+
+  // copy the rest of the fields in the record to the attrCacheEntry struct
+}
